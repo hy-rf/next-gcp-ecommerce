423ff17efe4eb84618fd4c7bc5910b77abfc286a
hy-rf <heyu880202@gmail.com>
2024-11-18 19:56:08 +0800

    fix: no key

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -2,6 +2,6 @@ import { Firestore } from "@google-cloud/firestore";
 export default function database() {
   return new Firestore({
     projectId: "e-commerce-442014",
-    // keyFilename: "./key.json"
+    keyFilename: "./key.json"
   });
 }
