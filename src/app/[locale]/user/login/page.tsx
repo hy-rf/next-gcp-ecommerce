3fe792bb4b1d4162f449bf244572e194cf677b03
hy-rf <heyu880202@gmail.com>
2024-11-18 19:46:14 +0800

    fix: save login redirect url in env

--- src/app/[locale]/user/login/page.tsx
+++ src/app/[locale]/user/login/page.tsx
@@ -2,7 +2,9 @@ export default async function Page() {
   return (
     <>
       <p>Login</p>
-      <a href="https://accounts.google.com/o/oauth2/v2/auth?client_id=981674263788-sfp6ddbl5pn1in3vmvgpbvre83bjigjm.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A3000/user/login-callback&response_type=token&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&include_granted_scopes=true&state=pass-through+value">
+      <a
+        href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=981674263788-sfp6ddbl5pn1in3vmvgpbvre83bjigjm.apps.googleusercontent.com&redirect_uri=${process.env.URL}/user/login-callback&response_type=token&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&include_granted_scopes=true&state=pass-through+value1`}
+      >
         Google login
       </a>
     </>
