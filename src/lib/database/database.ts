3cd878c2348070334a6a33b80107a70bb2c5fe89
hy-rf <heyu880202@gmail.com>
2024-11-19 21:12:04 +0800

    fix: using Application Default Credentials to access firestore on production

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -1,7 +1,13 @@
 import { Firestore } from "@google-cloud/firestore";
 export default function database() {
+  if (process.env.NODE_ENV === "production") {
+    return new Firestore();
+  } else {
+    console.log("The app is not running in production mode.");
+  }
+
   return new Firestore({
     projectId: "e-commerce-442014",
-    keyFilename: "./key.json"
+    keyFilename: "./key.json",
   });
 }
