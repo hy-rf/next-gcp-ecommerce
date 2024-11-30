18b49f4ccd2933d3bca148f2d3c63a1067066733
hy-rf <heyu880202@gmail.com>
2024-12-01 02:29:21 +0800

    feat: add filteredproducts component

--- src/app/[locale]/store/[id]/page.tsx
+++ src/app/[locale]/store/[id]/page.tsx
@@ -2,18 +2,30 @@ import fetchData from "@/lib/fetchData";
 import { Product, Store } from "@/model";
 import Image from "next/image";
 import AddToCartButton from "@/app/[locale]/product/_component/AddToCartButton";
+import FilteredProducts from "./_components/FilteredProducts";
 
 type Params = {
   id: string;
 };
+type SearchParams = {
+  page?: string;
+  filter: string;
+};
 
-export default async function Page({ params }: { params: Promise<Params> }) {
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: Promise<Params>;
+  searchParams: Promise<SearchParams>;
+}) {
   const { id } = await params;
+  const { page, filter } = await searchParams;
   const storeItem: Store = await fetchData<Store>(
-    `${process.env.URL}/api/store?id=${id}`,
+    `${process.env.URL}/api/store?id=${id}`
   );
   const products: Product[] = await fetchData<Product[]>(
-    `${process.env.URL}/api/product?storeId=${id}`,
+    `${process.env.URL}/api/product?storeId=${id}`
   );
 
   return (
@@ -32,36 +44,43 @@ export default async function Page({ params }: { params: Promise<Params> }) {
           <p>Owned by User ID: {storeItem.ownerUserId}</p>
         </div>
       </div>
-      <div className="space-y-4">
-        <h2 className="text-2xl font-semibold text-gray-800">Products</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {products.map((ele, index) => (
-            <div
-              key={index}
-              className="bg-white p-4 rounded-md shadow-product-card transition-shadow"
-            >
-              <Image
-                src={ele.imageUrl[0]}
-                alt={ele.name}
-                width={100}
-                height={100}
-                className="w-full h-40 object-cover rounded-md mb-4"
-              />
-              <a href={`/product/${ele.id}`}>
-                <h3 className="text-lg font-semibold text-gray-800">
-                  {ele.name}
-                </h3>
-              </a>
-              <p className="text-sm text-gray-600">{ele.description}</p>
-              <div className="text-sm text-gray-500 mt-2 space-y-1">
-                <p>Category: {ele.categoryId}</p>
-                <p>Subcategory: {ele.subCategoryId}</p>
+
+      {!filter && (
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold text-gray-800">Products</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {products.map((ele, index) => (
+              <div
+                key={index}
+                className="bg-white p-4 rounded-md shadow-product-card transition-shadow"
+              >
+                <Image
+                  src={ele.imageUrl[0]}
+                  alt={ele.name}
+                  width={100}
+                  height={100}
+                  className="w-full h-40 object-cover rounded-md mb-4"
+                />
+                <a href={`/product/${ele.id}`}>
+                  <h3 className="text-lg font-semibold text-gray-800">
+                    {ele.name}
+                  </h3>
+                </a>
+                <p className="text-sm text-gray-600">{ele.description}</p>
+                <div className="text-sm text-gray-500 mt-2 space-y-1">
+                  <p>Category: {ele.categoryId}</p>
+                  <p>Subcategory: {ele.subCategoryId}</p>
+                </div>
+                <AddToCartButton product={ele} />
               </div>
-              <AddToCartButton product={ele} />
-            </div>
-          ))}
+            ))}
+          </div>
+          <a href="">prev</a>
+          <p>{page}</p>
+          <a href="">next</a>
         </div>
-      </div>
+      )}
+      {filter && <FilteredProducts products={products} />}
     </div>
   );
 }
