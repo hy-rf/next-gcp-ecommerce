9fa20fc832404caebd38a2db662cde1ce9296b97
hy-rf <heyu880202@gmail.com>
2024-12-02 20:19:01 +0800

    chore: handle cart item fetch error

--- src/app/[locale]/_component/MobileCartItemList.tsx
+++ src/app/[locale]/_component/MobileCartItemList.tsx
@@ -8,8 +8,13 @@ export default function MobileCartItemList() {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
   useEffect(() => {
     (async () => {
-      const cartItems = await fetchData<CartItem[]>("/api/v2/cart-item");
-      setCartItems(cartItems);
+      try {
+        const cartItems = await fetchData<CartItem[]>("/api/v2/cart-item");
+        setCartItems(cartItems);
+      } catch {
+        console.log("No cart item or not log in");
+        setCartItems([]);
+      }
     })();
   }, []);
   async function handlePlusCartItem(cartItem: CartItem) {
