091945a88b73fd8c34126bc51965300527fcb1b7
hy-rf <heyu880202@gmail.com>
2024-12-03 19:56:36 +0800

    style: same width/height of image of product card

--- src/app/[locale]/product/_component/FilteredProducts.tsx
+++ src/app/[locale]/product/_component/FilteredProducts.tsx
@@ -61,7 +61,7 @@ export default function FilteredProducts({
   }, [options]);
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
         {filteredProducts.map((ele) => (
           <ProductItem ele={ele} key={ele.id} />
         ))}
--- src/app/[locale]/product/_component/ProductItem.tsx
+++ src/app/[locale]/product/_component/ProductItem.tsx
@@ -5,30 +5,24 @@ import AddToCartButton from "./AddToCartButton";
 
 export default function ProductItem({ ele }: { ele: Product }) {
   return (
-    <div className="flex bg-white p-4 rounded-md shadow-product-card transition-shadow">
-      <div>
+    <div className="flex bg-white p-4 rounded-md shadow-product-card transition-shadow w-full gap-4">
+      <div className="w-40 h-40 flex-shrink-0">
         <Image
           src={ele.imageUrl[0]}
           alt={ele.name}
-          width={100}
-          height={100}
-          className="w-full h-40 object-cover rounded-md mb-4"
+          width={160}
+          height={160}
+          className="w-full h-full object-cover rounded-md"
         />
       </div>
-      <div>
+      <div className="flex flex-col justify-between">
         <a href={`/product/${ele.id}`}>
           <h3 className="text-lg font-semibold text-gray-800">{ele.name}</h3>
         </a>
         <p className="text-sm text-gray-600">{ele.description}</p>
         <p className="text-xl font-bold text-green-600">${ele.price}</p>
-        <p className="text-xl font-bold text-green-600">Sold: {ele.sold}</p>
-        <p className="text-xl font-bold text-green-600">
-          In Stock: {ele.stock}
-        </p>
-        <div className="text-sm text-gray-500 mt-2 space-y-1">
-          <p>Category: {ele.categoryId}</p>
-          <p>Subcategory: {ele.subCategoryId}</p>
-        </div>
+        <p className="text-md text-gray-700">Sold: {ele.sold}</p>
+        <p className="text-md text-gray-700">In Stock: {ele.stock}</p>
         <AddToCartButton product={ele} />
       </div>
     </div>
