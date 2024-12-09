6b227a70d2fd5182d7c3c787681c1ad65c14c6f7
hy-rf <heyu880202@gmail.com>
2024-12-09 19:41:33 +0800

    fix: remove refresh token at logout

--- src/app/[locale]/user/logout/page.tsx
+++ src/app/[locale]/user/logout/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 export default function Logout() {
   const handleLogout = () => {
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie =
+      "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;refresh=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.href = "/";
   };
   return (
