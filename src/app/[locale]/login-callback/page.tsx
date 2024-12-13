ee06c6ea8597af6077665256bbaad6c640e2308a
hy-rf <heyu880202@gmail.com>
2024-12-13 22:02:50 +0800

    chore: move oauth callback from /[locale]/user/login-callback to /[locale]/login-callback

--- src/app/[locale]/login-callback/page.tsx
+++ src/app/[locale]/login-callback/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
   // TODO: process token from url then POST /user/login/api
   return (
     <>
-      <p>Processing data...</p>
+      <p>Processing...</p>
       <ClientProcessingUserLogin />
     </>
   );
