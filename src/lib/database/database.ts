5bdd97637996359f683c3ea6e65a256f71c58191
hy-rf <heyu880202@gmail.com>
2024-12-15 00:10:05 +0800

    feat: add sharp to solve image loading issue

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -1,10 +1,5 @@
 import { Firestore } from "@google-cloud/firestore";
 
 export default function database() {
-  return new Firestore({
-    credentials: {
-      client_email: process.env.CE,
-      private_key: process.env.PK,
-    },
-  });
+  return new Firestore();
 }
--- src/lib/database/storage.ts
+++ src/lib/database/storage.ts
@@ -1,10 +1,5 @@
 import { Storage } from "@google-cloud/storage";
 
 export default function storage() {
-  return new Storage({
-    credentials: {
-      client_email: process.env.CE,
-      private_key: process.env.PK,
-    },
-  });
+  return new Storage();
 }
