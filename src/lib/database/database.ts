a02984033a61e6ec22561198be32522221554eff
hy-rf <heyu880202@gmail.com>
2024-12-14 22:25:14 +0800

    chore: compatible to vercel

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -1,5 +1,10 @@
 import { Firestore } from "@google-cloud/firestore";
 
 export default function database() {
-  return new Firestore();
+  return new Firestore({
+    credentials: {
+      client_email: process.env.CE,
+      private_key: process.env.PK,
+    },
+  });
 }
--- src/lib/database/storage.ts
+++ src/lib/database/storage.ts
@@ -1,5 +1,10 @@
 import { Storage } from "@google-cloud/storage";
 
 export default function storage() {
-  return new Storage();
+  return new Storage({
+    credentials: {
+      client_email: process.env.CE,
+      private_key: process.env.PK,
+    },
+  });
 }
