dcbfb0a321642c8a280a1eed5274fb7d99e72fb3
hy-rf <heyu880202@gmail.com>
2024-12-14 20:13:53 +0800

    feat: remove carousel to ignore image loading too slow problem

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import CategoryList from "./_component/CategoryList";
-import Hero from "./_component/Hero";
+// import Hero from "./_component/Hero";
 import getDictionary from "@/dictionary/dictionary";
 import { Category } from "@/model";
 import fetchData from "@/lib/fetchData";
@@ -14,7 +14,7 @@ export default async function Page(props: { params: Params }) {
   )) as Category[];
   return (
     <>
-      <Hero />
+      {/* <Hero /> */}
       <p className="hidden">{(await props.params).locale}</p>
       <div className="text-center px-4 py-2 border box-content rounded-lg mt-2 mx-auto w-max shadow-sm">
         <Link href="/product?page=1&sort=sold-desc">
