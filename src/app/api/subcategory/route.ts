970338090f7ab1da911978cb62d0a87f58298865
hy-rf <heyu880202@gmail.com>
2024-12-17 17:37:53 +0800

    chore: move getting all subcategories to end of "GET" /api/subcategory api function and move getting subcategories by category id to start of  "GET" /api/subcategory api function.

--- src/app/api/subcategory/route.ts
+++ src/app/api/subcategory/route.ts
@@ -1,12 +1,19 @@
 import database from "@/lib/database/database";
 import { NextRequest } from "next/server";
-
+/**
+ * Get sub categories by category id or all
+ */
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const categoryId = searchParams.get("id");
   const db = database();
-  if (!categoryId) {
-    const subCategorySnapshot = (await db.collection("SubCategory").get()).docs;
+  if (categoryId) {
+    const subCategorySnapshot = (
+      await db
+        .collection("SubCategory")
+        .where("categoryId", "==", categoryId)
+        .get()
+    ).docs;
     return Response.json(
       subCategorySnapshot.map((ele) => {
         return {
@@ -16,12 +23,7 @@ export async function GET(req: NextRequest) {
       })
     );
   }
-  const subCategorySnapshot = (
-    await db
-      .collection("SubCategory")
-      .where("categoryId", "==", categoryId)
-      .get()
-  ).docs;
+  const subCategorySnapshot = (await db.collection("SubCategory").get()).docs;
   return Response.json(
     subCategorySnapshot.map((ele) => {
       return {
