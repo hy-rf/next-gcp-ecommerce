7c9f8c5c81f0cc04ae0048e8d181924ad9f9298e
hy-rf <heyu880202@gmail.com>
2024-12-17 16:14:45 +0800

    chore: rename param part to namespace in getdictionary

--- src/dictionary/dictionary.ts
+++ src/dictionary/dictionary.ts
@@ -3,14 +3,14 @@ import { Dictionary } from "@/model";
 
 export default async function getDictionary(
   locale: string,
-  part: string
+  namespace: string
 ): Promise<Dictionary> {
   switch (locale) {
     case "en-US":
-      return (await import(`./en/${part}.ts`)).default;
+      return (await import(`./en/${namespace}.ts`)).default;
     case "zh-TW":
-      return (await import(`./zh-TW/${part}.ts`)).default;
+      return (await import(`./zh-TW/${namespace}.ts`)).default;
     default:
-      return (await import(`./en/${part}.ts`)).default;
+      return (await import(`./en/${namespace}.ts`)).default;
   }
 }
