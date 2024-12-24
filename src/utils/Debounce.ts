6688a1477b17b6c5e8598f883fc019b64ef725b2
hy-rf <heyu880202@gmail.com>
2024-12-25 03:49:48 +0800

    chore: add debounce util

---
+++ src/utils/Debounce.ts
@@ -0,0 +1,22 @@
+type DebouncedFunction<T extends (...args: unknown[]) => unknown> = (
+  ...args: Parameters<T>
+) => void;
+
+function debounce<T extends (...args: unknown[]) => unknown>(
+  callback: T,
+  delay: number
+): DebouncedFunction<T> {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>): void => {
+    // Clear the previous timer
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    // Set a new timer
+    timer = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
