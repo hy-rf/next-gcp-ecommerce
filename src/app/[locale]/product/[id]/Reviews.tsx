b3fa945f811277787854f1389f3ab02774a3752a
hy-rf <heyu880202@gmail.com>
2024-12-25 11:37:10 +0800

    style: add stars in product review list

--- src/app/[locale]/product/[id]/Reviews.tsx
+++ src/app/[locale]/product/[id]/Reviews.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { Review } from "@/model";
 
 export default function Reviews({ reviews }: { reviews: Review[] }) {
@@ -8,11 +9,42 @@ export default function Reviews({ reviews }: { reviews: Review[] }) {
     <div className="bg-white rounded-lg shadow-md p-6">
       <h5>Reviews</h5>
       {reviews.map((el) => {
+        const userId =
+          el.userId[0] +
+          "*".repeat(el.userId.length / 2) +
+          el.userId[el.userId.length - 1];
         return (
           <div key={el.id}>
-            <p>{el.stars}</p>
+            <span className="inline-block">Reviewer: </span>
+            <span className="text-gray-600"> {userId}</span>
+            <div className="flex">
+              {[0, 1, 2, 3, 4].map((ele, i) => {
+                if (ele < el.stars) {
+                  return (
+                    <div key={i} className={`w-4 h-4`}>
+                      <Image
+                        src={"/star-fill.svg"}
+                        alt={"fill"}
+                        width={64}
+                        height={64}
+                      ></Image>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div key={i} className={`w-4 h-4`}>
+                      <Image
+                        src={"/star-empty.svg"}
+                        alt={"empty"}
+                        width={64}
+                        height={64}
+                      ></Image>
+                    </div>
+                  );
+                }
+              })}
+            </div>
             <p>{el.content}</p>
-            <p>{el.userId}</p>
           </div>
         );
       })}
